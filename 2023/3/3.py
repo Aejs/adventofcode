cbed314d9c20df06c6cb2136dc7397550026ac4b
Alexander Etienne Josef Schlingmann <kontakt@alexander-schlingmann.de>
2023-12-17 12:27:36 +0100

    Übertrag 17.12.2023

---
+++ 2023/3/3.py
@@ -0,0 +1,67 @@
+import re
+with open("input", 'r') as f:
+    lines = {}
+    ln = 1
+    for line in f.readlines():
+        line = "." + line + "."
+        lines[ln] = line
+        ln += 1
+guetige_zahlen = []
+for i in range(1, 101):
+    if i-1 >= 1:
+        last_line = lines[i-1].strip()
+    else:
+        last_line = 140*"."
+    cur_line = lines[i].strip()
+    if i-1 <= 100:
+        next_line = lines[i+1].strip()
+    else:
+        next_line = 142*"."
+    print(last_line)
+    print(cur_line)
+    print(next_line)
+    print("\n")
+    print("\n")
+    zahlen = []
+    akt_zahl = []
+    position = 1
+    for i in cur_line:
+        if i.isdigit():
+            if akt_zahl == []:
+                akt_zahl.append(position)
+        else:
+            if akt_zahl != []:
+                akt_zahl.append(position-1)
+                zahlen.append(akt_zahl)
+                akt_zahl = []
+        position += 1
+    for zahl in zahlen:
+        werten = False
+        zahl_wert = []
+
+        if cur_line[zahl[0]-2] != ".":
+            werten = True
+        if cur_line[zahl[1]] != ".":
+            werten = True
+
+        for i in range(zahl[0]-1, zahl[1]):
+            if last_line[i] is not ".":
+                if last_line[i].isdigit() is False:
+                    werten = True
+            if next_line[i] is not ".":
+                if next_line[i].isdigit() is False:
+                    werten = True
+
+        if werten is True:
+            for i in range(zahl[0]-1, zahl[1]):
+                zahl_wert.append(cur_line[i])
+            guetige_zahlen.append(int("".join(zahl_wert)))
+            zahl_wert = []
+    print(guetige_zahlen)
+    input()
+print(guetige_zahlen)
+sum = 0
+for i in guetige_zahlen:
+    sum = sum + i
+print(sum)
+# input()
